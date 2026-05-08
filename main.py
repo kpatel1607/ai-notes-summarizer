3c5806f175800eccae82ddb56a1b0c5bb44b9f4d
kpatel1607 <143311915+kpatel1607@users.noreply.github.com>
2026-05-08 15:30:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -154,7 +154,7 @@ def summarize_notes(request: NoteRequest, x_api_key: str = Header(None)):
     """
 
     response = client.chat.completions.create(
-        model="meta-llama/llama-3.3-70b-instruct:free",
+        model="deepseek/deepseek-chat-v3-0324:free",
         temperature=0.3,
         messages=[
             {
